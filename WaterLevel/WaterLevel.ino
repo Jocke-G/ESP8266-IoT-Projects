5b227074ab875025f403edefc983ff1001d535d6
Jocke-G <joakim.gustafsson.88@gmail.com>
2018-05-09 22:48:11 +0200

    Update WaterLevel.ino

--- WaterLevel/WaterLevel.ino
+++ WaterLevel/WaterLevel.ino
@@ -1,27 +1,30 @@
-#include <NewPing.h>
+#include <NewPingESP8266.h> //https://github.com/jshaw/NewPingESP8266
 #include "ConnectionManager.h"
 
-#define loopInterval 2000
+#define loopInterval 10000
 #define waterReserveTopic "WaterReserve"
-
-#define RESET_PIN 0
-#define TRIG_PIN 12
-#define ECHO_PIN 12
 
 const float BARREL_HEIGHT = 85+10+20;
 const float BARREL_DIAMETER = 57;
 const float BARREL_RADIUS = BARREL_DIAMETER / 2;
 const float BARREL_AREA = BARREL_RADIUS * BARREL_RADIUS * PI;
 
-
+#define RESET_PIN 0
+#define TRIG_PIN 12
+#define ECHO_PIN 16
 
 unsigned long lastLoop = 0;
 
-NewPing sonar(TRIG_PIN, ECHO_PIN);
+NewPingESP8266 sonar(TRIG_PIN, ECHO_PIN);
 ConnectionManager connection(RESET_PIN);
 
 void messageReceived(String &topic, String &payload) {
   Serial.println("Incoming: " + topic + " - " + payload);
+}
+
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  connection.shouldSaveConfig = true;
 }
 
 void setup() {
@@ -29,6 +32,7 @@ void setup() {
   Serial.println("Setup...");
 
   connection.setMessageCallback(messageReceived);
+  connection.setSaveCallback(saveConfigCallback);
   connection.setup();
   Serial.println("Setup Done");
 }
@@ -46,19 +50,16 @@ void loop() {
 }
 
 void readSonarSensor(){
-  unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
-  
-  float distance = (float)uS / US_ROUNDTRIP_CM;
+  Serial.print("Ping: ");
+  float distance = sonar.ping_cm();
+  Serial.print(sonar.ping_cm());
+  Serial.print(" cm, ");
+
   float waterLevel = BARREL_HEIGHT - distance;
   float volumeCM = waterLevel * BARREL_AREA;
   float liters = volumeCM / 1000;
-
-  Serial.print("Ping: ");
+  
   Serial.print(liters);
-  Serial.println("cm");
-  connection.publish(waterReserveTopic, String(volumeCM).c_str());
-}
-
-bool wifiConnected() {
-  return WiFi.status() == WL_CONNECTED;
+  Serial.println(" liters");
+  connection.publish(waterReserveTopic, String(liters).c_str());
 }
